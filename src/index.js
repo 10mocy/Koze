1489fcae3f943ef90281824ba70a6acd74163eee
Tomotada Saigusa <nirot1r@g-second.net>
2019-11-04 11:09:44 +0900

    v1.00

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ require('dotenv').config()
 
 const main = () => {
   if (!process.env.BOT_TOKEN) throw new Error('❌ BOTトークンが入力されていません。')
-  if (!process.env.CHANNEL_ID) throw new Error('❌ チャンネルIDが入力されていません。')
 
   client.login(process.env.BOT_TOKEN).catch(err => console.error(err))
 
@@ -16,6 +15,17 @@ const main = () => {
   })
 
   client.on('message', msg => {
+    if (msg.author.id === client.user.id) return
+    if (msg.channel.type !== 'text') return
+
+    if (msg.content === '\\vclink') {
+      if (!msg.member.voiceChannelID) {
+        msg.channel.send('VCに接続してからコマンド実行してね！')
+        return
+      }
+
+      msg.channel.send(`https://discordapp.com/channels/${msg.guild.id}/${msg.member.voiceChannelID}`)
+    }
   })
 
   client.on('error', err => console.error(err))
